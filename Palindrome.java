adb8a6f0f352b826e6f1d737f18014d9dcdc33df
anandgru <111253707+anandgru@users.noreply.github.com>
2024-05-25 17:56:28 +0530

    Changed to only Reverse LL

--- Palindrome.java
+++ Palindrome.java
@@ -1,32 +1,15 @@
 class Solution {
     ListNode reverse(ListNode head){
         ListNode c=head;
-        ListNode n=head;
+        ListNode n=null;
         ListNode p=null;
         while(c!=null){
-            n=n.next;
+            n=c.next;
             c.next=p;
             p=c;
             c=n;
         }
         return p;
     }
-    public boolean isPalindrome(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-         while(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-         }
-         ListNode t=reverse(slow);
-         ListNode t1=head;
-         while(t!=null){
-            if(t1.val!=t.val){
-                 return false;
-            }
-            t=t.next;
-            t1=t1.next;
-         }
-     return true;   
-    }
+    
 }
